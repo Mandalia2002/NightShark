7c3292dbe84e24fc28047b8bdd1e874b9127fcef
Mandalia2002 <sofiapg.2002@hotmail.com>
2025-05-05 16:28:59 -0700

    si.

--- Nightly/src/app/tracking/tracking.component.ts
+++ Nightly/src/app/tracking/tracking.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-tracking',
-  imports: [],
+  imports: [
+    MatProgressBarModule,
+    MatProgressSpinnerModule
+  ],
   templateUrl: './tracking.component.html',
-  styleUrl: './tracking.component.css'
+  styleUrl: './tracking.component.css',
+  template: `
+    <a class="btnPro" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false"aria-controls="collapseExample">Amanecer</a>`,
 })
 export class TrackingComponent {
-
+  btnPro(event: MouseEvent): void
+  if (eventey==) {
+    
+  }
 }
