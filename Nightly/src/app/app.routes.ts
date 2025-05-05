2e9dbe775d6495234a3588444238277dfcb6e271
Mandalia2002 <sofiapg.2002@hotmail.com>
2025-05-05 12:02:13 -0700

    Inicio Front Terminado

--- Nightly/src/app/app.routes.ts
+++ Nightly/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { InformeComponent } from './informe/informe.component';
+import { PesoComponent } from './peso/peso.component';
+import { TrackingComponent } from './tracking/tracking.component';
+import { VoidComponent } from './void/void.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: InicioComponent},
+    {path: 'Inicio', component: InicioComponent},
+    {path: 'Informe', component: InformeComponent},
+    {path: 'Peso', component: PesoComponent},
+    {path: 'Tracking', component: TrackingComponent},
+    {path: '**', component: VoidComponent},
+];
