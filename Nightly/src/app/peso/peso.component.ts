d83c07c692b48c929740fda59926a4b3340dcae3
Mandalia2002 <sofiapg.2002@hotmail.com>
2025-05-05 09:05:21 -0700

    Inicio Beta

---
+++ Nightly/src/app/peso/peso.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-peso',
+  imports: [],
+  templateUrl: './peso.component.html',
+  styleUrl: './peso.component.css'
+})
+export class PesoComponent {
+
+}
