dc879d83399a9b1b02d8e5d4ffb12c4cd15b9ac3
Mandalia2002 <sofiapg.2002@hotmail.com>
2025-05-06 11:03:43 -0700

    aaa

--- Nightly/src/app/tracking/tracking.component.ts
+++ Nightly/src/app/tracking/tracking.component.ts
@@ -1,21 +1,32 @@
-import { Component } from '@angular/core';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { Component, ChangeDetectionStrategy, signal, inject } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-tracking',
   imports: [
     MatProgressBarModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    JsonPipe,
+    MatCheckboxModule
   ],
   templateUrl: './tracking.component.html',
-  styleUrl: './tracking.component.css',
-  template: `
-    <a class="btnPro" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false"aria-controls="collapseExample">Amanecer</a>`,
+  styleUrl: './tracking.component.css'
 })
 export class TrackingComponent {
-  btnPro(event: MouseEvent): void
-  if (eventey==) {
-    
-  }
+  private readonly _formBuilder = inject(FormBuilder);
+
+  readonly amanecerS = this._formBuilder.group({
+    Hora: false,
+    Cama: false,
+    Escritorio: false,
+    Dientes: false,
+    Cara: false,
+    Cuerpo: false
+  });
 }
