86d8c420eee62021b78c439b9eca34249155300a
Mandalia2002 <sofiapg.2002@hotmail.com>
2025-05-14 15:28:02 -0700

    Graficas de Pantalla 2, joder que chingona soy

--- Nightly/src/app/tracking/tracking.component.ts
+++ Nightly/src/app/tracking/tracking.component.ts
@@ -1,38 +1,73 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, NgModule, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-tracking',
   imports: [
-    FormsModule,
-    ReactiveFormsModule,
     MatCheckboxModule,
     NgbProgressbarModule,
+    CommonModule,
   ],
   templateUrl: './tracking.component.html',
   styleUrl: './tracking.component.css',
 })
 
-export class TrackingComponent {
-  graf = false;
-  ocultar() {
-    this.graf = true;
+export class TrackingComponent implements AfterViewInit{
+  progLuz = 40;
+  progDia = 30;
+  progNoc = 80;
+  graf1 = false;
+  graf2 = false;
+  graf3 = false;
+
+  @ViewChild('dropdownCollapseRef1') dropdownCollapseRef1!: ElementRef;
+  @ViewChild('dropdownCollapseRef2') dropdownCollapseRef2!: ElementRef;
+  @ViewChild('dropdownCollapseRef3') dropdownCollapseRef3!: ElementRef;
+
+  ngAfterViewInit() {
+    const el1 = this.dropdownCollapseRef1.nativeElement;
+    const el2 = this.dropdownCollapseRef2.nativeElement;
+    const el3 = this.dropdownCollapseRef3.nativeElement;
+
+    el1.addEventListener('show.bs.collapse', () =>{
+      this.graf1 = true;
+    })
+
+    el1.addEventListener('hide.bs.collapse', ()=>{
+      this.graf1 = false;
+    })
+
+    el2.addEventListener('show.bs.collapse', () =>{
+      this.graf2 = true;
+    })
+
+    el2.addEventListener('hide.bs.collapse', ()=>{
+      this.graf2 = false;
+    })
+    
+    el3.addEventListener('show.bs.collapse', () =>{
+      this.graf3 = true;
+    })
+
+    el3.addEventListener('hide.bs.collapse', ()=>{
+      this.graf3 = false;
+    })
   }
 
-  mostrar() {
-    this.graf = false;
-  }
+   
 
-  private readonly _formBuilder = inject(FormBuilder);
 
-  readonly amanecerS = this._formBuilder.group({
-    Hora: 1,
-    Cama: 1,
-    Escritorio: 1,
-    Dientes: 1,
-    Cara: 1,
-    Cuerpo: 1
-  });
+
+  // private readonly _formBuilder = inject(FormBuilder);
+
+  // readonly amanecerS = this._formBuilder.group({
+  //   Hora: 1,
+  //   Cama: 1,
+  //   Escritorio: 1,
+  //   Dientes: 1,
+  //   Cara: 1,
+  //   Cuerpo: 1
+  // });
 }
