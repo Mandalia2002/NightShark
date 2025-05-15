bbf7ec499725d8cbf925376430e0c27e0050e293
Mandalia2002 <sofiapg.2002@hotmail.com>
2025-05-15 16:13:50 -0700

    Front ACABADO AAAAA Inicio del Back :3

--- Nightly/src/app/tracking/tracking.component.ts
+++ Nightly/src/app/tracking/tracking.component.ts
@@ -2,6 +2,7 @@ import { Component, NgModule, ViewChild, ElementRef, AfterViewInit } from '@angu
 import { CommonModule } from '@angular/common';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tracking',
@@ -25,6 +26,11 @@ export class TrackingComponent implements AfterViewInit{
   graf2 = false;
   graf3 = false;
 
+  grafM1 = false;
+  grafM2 = false;
+  grafM3 = false;
+
+
   @ViewChild('dropdownCollapseRef1') dropdownCollapseRef1!: ElementRef;
   @ViewChild('dropdownCollapseRef2') dropdownCollapseRef2!: ElementRef;
   @ViewChild('dropdownCollapseRef3') dropdownCollapseRef3!: ElementRef;
@@ -36,41 +42,55 @@ export class TrackingComponent implements AfterViewInit{
 
     el1.addEventListener('show.bs.collapse', () =>{
       this.graf1 = true;
+      this.grafM1 = true;
     })
 
     el1.addEventListener('hide.bs.collapse', ()=>{
       this.graf1 = false;
+      this.grafM1 = false;
     })
 
     el2.addEventListener('show.bs.collapse', () =>{
       this.graf2 = true;
+      this.grafM2 = true;
     })
 
     el2.addEventListener('hide.bs.collapse', ()=>{
       this.graf2 = false;
+      this.grafM2 = false;
     })
     
     el3.addEventListener('show.bs.collapse', () =>{
       this.graf3 = true;
+      this.grafM3 = true;
     })
 
     el3.addEventListener('hide.bs.collapse', ()=>{
       this.graf3 = false;
+      this.grafM3 = false;
     })
   }
 
-   
+//////////////////
 
+valor: string ='';
 
+Amanecer(valor1:string){
+  this.valor=valor1;
+}
 
-  // private readonly _formBuilder = inject(FormBuilder);
+Dia(valor1:string){
+  this.valor=valor1;
+}
 
-  // readonly amanecerS = this._formBuilder.group({
-  //   Hora: 1,
-  //   Cama: 1,
-  //   Escritorio: 1,
-  //   Dientes: 1,
-  //   Cara: 1,
-  //   Cuerpo: 1
-  // });
+Noche(valor1:string){
+  this.valor=valor1;
+}
+
+enviar(valor2:string){
+  console.log(valor2);
+ //this.HttpClient.post('http://localhost:3000/api/estado', {valor: valor2}).subscribe(arg => this.property = arg);
+  
+}
+
 }
