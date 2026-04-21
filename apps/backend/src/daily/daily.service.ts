a82b1e1f7af6d97743633f8a1e712e326b054641
Mandalia2002 <sofiapg.2002@hotmail.com>
2026-04-21 09:35:42 -0700

    Tests

--- apps/backend/src/daily/daily.service.ts
+++ apps/backend/src/daily/daily.service.ts
@@ -16,7 +16,12 @@ export class DailyService {
 
   async createNewDay() {
     const dateNow = new Date()
-    console.log(dateNow)
+    const formatter = new Intl.DateTimeFormat('en-US', {
+      timeZone: 'America/Tijuana',
+      dateStyle: 'short',
+      timeStyle: 'short',
+    });
+    console.log(formatter.format(dateNow))
     dateNow.setHours(0, 0, 0, 0)
     console.log(dateNow)
     const sun = await this.DailyRepository.findOne({ where: { date: dateNow } })
